fca50961f08b496853dec3f228d7652219348c5b
Jacob Erlbeck <jacob@wgtwo.com>
2023-09-04 11:56:12 +0200

    Fix name clashes / Remove base_name option
    Currently `{rename,{msg_fqname,base_name}}` is always added to the gpb
    options, causing duplicate names to clash even if they come from different
    packages.
    
    Remove the option and fix the templates so that they support arbitrary atom
    names.

--- priv/grpcbox_service_bhvr.erl
+++ priv/grpcbox_service_bhvr.erl
@@ -5,11 +5,13 @@
 
 %% this module was generated and should not be modified manually
 
+%% TEST
+
 -module({{module_name}}_bhvr).
 
 {{#methods}}
 %% {{^input_stream}}{{^output_stream}}Unary RPC{{/output_stream}}{{/input_stream}}
--callback {{method}}({{^input_stream}}{{#output_stream}}{{pb_module}}:{{input}}(), grpcbox_stream:t(){{/output_stream}}{{/input_stream}}{{#input_stream}}{{^output_stream}}reference(), grpcbox_stream:t(){{/output_stream}}{{#output_stream}}reference(), grpcbox_stream:t(){{/output_stream}}{{/input_stream}}{{^input_stream}}{{^output_stream}}ctx:t(), {{pb_module}}:{{input}}(){{/output_stream}}{{/input_stream}}) ->
-    {{#output_stream}}ok{{/output_stream}}{{^output_stream}}{ok, {{pb_module}}:{{output}}(), ctx:t()} | {noreply, ctx:ctx()}{{/output_stream}} | grpcbox_stream:grpc_error_response().
+-callback {{method}}({{^input_stream}}{{#output_stream}}'{{pb_module}}':'{{input}}'(), grpcbox_stream:t(){{/output_stream}}{{/input_stream}}{{#input_stream}}{{^output_stream}}reference(), grpcbox_stream:t(){{/output_stream}}{{#output_stream}}reference(), grpcbox_stream:t(){{/output_stream}}{{/input_stream}}{{^input_stream}}{{^output_stream}}ctx:t(), '{{pb_module}}':'{{input}}'(){{/output_stream}}{{/input_stream}}) ->
+    {{#output_stream}}ok{{/output_stream}}{{^output_stream}}{ok, '{{pb_module}}':'{{output}}'(), ctx:t()} | {noreply, ctx:ctx()}{{/output_stream}} | grpcbox_stream:grpc_error_response().
 
 {{/methods}}
